1f73953538411f109ab748e351d95f44c469022f
LuSanchess <luisa.silva@anglobauru.com.br>
2024-10-22 10:45:44 -0300

    incompleto, pegar o resto de alguem cadastrar usuarios e terminar forms usuarios

---
+++ MapaSala/Classes/Usuarios.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapaSala.Classes
+{
+    public class Usuarios
+    {
+       
+        private SqlConnection Conexao = new SqlConnection ("Server = LS05MPF; Database=AULA_DS;User Id = sa; Password=admsasql;");// link do site /comunicacao programa/banco
+
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Senha { get; set; }
+        public bool Ativo { get; set; }
+
+        public void Inserir()
+        {
+            Conexao.Open();
+            string query = "insert into Cursos(Nome, Turno,Sigla, Ativo) Values (@nome, @turno,@sigla,@ativo)";
+            SqlCommand comando = new SqlCommand(query, Conexao);
+
+            SqlParameter parametro1 = new SqlParameter("@login",Login);
+            SqlParameter parametro2 = new SqlParameter("@senha", Senha);
+            SqlParameter parametro3 = new SqlParameter("@ativo",Ativo);
+
+            comando.Parameters.Add(parametro1);
+            comando.Parameters.Add(parametro2);
+            comando.Parameters.Add(parametro3);
+          
+            comando.ExecuteNonQuery(); //nao retorna nd
+            Conexao.Close();
+        }
+    }
+}
--- MapaSala/Formularios/frmUsuarios.Designer.cs
+++ MapaSala/Formularios/frmUsuarios.Designer.cs
@@ -28,12 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnNovo = new System.Windows.Forms.Button();
+            this.DtGridUsuarios = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.DtGridUsuarios)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnNovo
+            // 
+            this.btnNovo.Location = new System.Drawing.Point(6, 73);
+            this.btnNovo.Name = "btnNovo";
+            this.btnNovo.Size = new System.Drawing.Size(75, 23);
+            this.btnNovo.TabIndex = 0;
+            this.btnNovo.Text = "Novo";
+            this.btnNovo.UseVisualStyleBackColor = true;
+            // 
+            // DtGridUsuarios
+            // 
+            this.DtGridUsuarios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DtGridUsuarios.Location = new System.Drawing.Point(6, 166);
+            this.DtGridUsuarios.Name = "DtGridUsuarios";
+            this.DtGridUsuarios.Size = new System.Drawing.Size(391, 229);
+            this.DtGridUsuarios.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.DtGridUsuarios);
+            this.groupBox1.Controls.Add(this.btnNovo);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(424, 401);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // frmUsuarios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmUsuarios";
             this.Text = "frmUsuarios";
+            ((System.ComponentModel.ISupportInitialize)(this.DtGridUsuarios)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnNovo;
+        private System.Windows.Forms.DataGridView DtGridUsuarios;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
